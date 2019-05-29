1e2cc7da8e7f5b930adc46a03d50309260b4dedb
Manuel Coutinho <manuel.monge.coutinho@gmail.com>
2019-05-29 16:26:16 +0100

    Modified sphere

--- Project 2/shapes/MySphere.js
+++ Project 2/shapes/MySphere.js
@@ -23,36 +23,17 @@ class MySphere extends CGFobject {
 		for(var j = 0; j <= this.stacks; j++){
 			
 			for(var i = 0; i <= this.slices; i++){
-
-				this.vertices.push(Math.cos(thetaAng) * Math.cos(phiAng), Math.sin(thetaAng) * Math.cos(phiAng),-Math.sin(phiAng));
+		
+				this.vertices.push(Math.cos(thetaAng) * Math.cos(phiAng), -Math.sin(phiAng), -Math.cos(phiAng) * Math.sin(thetaAng));
 				
-				this.normals.push(Math.cos(thetaAng) * Math.cos(phiAng), Math.sin(thetaAng) * Math.cos(phiAng),- Math.sin(phiAng));
-
+				this.normals.push(Math.cos(thetaAng) * Math.cos(phiAng), -Math.sin(phiAng), -Math.cos(phiAng) * Math.sin(thetaAng));
+		
 				this.texCoords.push( i /this.slices, j / this.stacks);
 
 				thetaAng+=thetaInc;
 			}
 			phiAng += phiInc;
 		}
-		/*
-		var thetaAng = 0;
-		var phiAng = 0;
-
-		for(var j = 0; j <= this.stacks; j++){
-			
-			for(var i = 0; i <= this.slices; i++){
-
-				this.vertices.push(-Math.cos(thetaAng) * Math.cos(phiAng), Math.sin(thetaAng) * Math.cos(phiAng), Math.sin(phiAng));
-				
-				this.normals.push(-Math.cos(thetaAng) * Math.cos(phiAng), Math.sin(thetaAng) * Math.cos(phiAng), Math.sin(phiAng));
-
-				this.texCoords.push( i /this.slices, j / this.stacks);
-
-				thetaAng+=thetaInc;
-			}
-			phiAng += phiInc;
-		}
-*/
 		
 
 		for(var j = 0; j < this.stacks; j++){
@@ -63,14 +44,7 @@ class MySphere extends CGFobject {
 				
 			}
 		}
-/*
-		for(var j = this.stacks; j <= this.stacks * 2 ; j++){
-			for (var i = this.slices; i <= this.slices * 2; i++) {
-				this.indices.push((i + 1) * (this.slices + 1) + j, i * (this.slices + 1) +  1 + j, i * (this.slices + 1) + j);
-				this.indices.push( (i + 1) * (this.slices + 1) + j , (i + 1) * (this.slices + 1) + 1 + j , i * (this.slices + 1) +  1 + j);
-			}
-		}
-*/
+
 		this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
 	}
